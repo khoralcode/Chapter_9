caad25519dd39a40e9607174a2903b11aa8e0f5e
Alex <macbookpro@akmbp.workgroup>
2014-09-23 15:15:31 -0700

    Initial Commit

---
+++ Chapter_9/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Chapter_9
+//
+//  Created by Alex on 9/23/14.
+//  Copyright (c) 2014 ObjCBook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
